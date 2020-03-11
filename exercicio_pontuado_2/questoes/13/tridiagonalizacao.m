f90662ceb276b21723f80a660fbb490c68b57d19
Neithan <nathangomes@mat.ci.ufpb.br>
2020-03-11 13:47:41 -0300

    update matrizes and tridiag

--- exercicio_pontuado_2/questoes/13/tridiagonalizacao.m
+++ exercicio_pontuado_2/questoes/13/tridiagonalizacao.m
@@ -1,17 +1,17 @@
 function T = tridiagonalizacao (A)
   T = A;
   [m n] = size(A);
+  n = max(size(A))
   
-  for i = 1:n-2
-    x = T((i+1):n,i);
-    sx = size(x,1);
-    y = zeros(sx,1);
+  for i = 2:n-1
+    x = T(i:n, i-1);
+    y = zeros(n-i+1,1);
     y(1) = norm(x);
-    w = x - y;
-    Q = eye(sx) - 2*(w * w')/(w' * w);
-    T((i+1):n,i) = y;
-    T(i,(i+1):n) = y;
-    T((i+1):n,(i+1):n) = Q * T((i+1):n,(i+1):n) * Q;
+    v = x - y;
+    Q = eye(n-i+1) - 2*(v * v')/(v' * v);
+    T(i:n, i-1) = y;
+    T(i-1, i:n) = y;
+    T(i:n,i:n) = Q * T(i:n, i:n) * Q;
     
   endfor
 endfunction
